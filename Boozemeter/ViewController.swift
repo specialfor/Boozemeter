2c544025e33f36e10e6a35e3a35f485346e05d1e
Volodymyr Hryhoriev <v.hryhoriev@tecsynt.com>
2017-11-04 11:11:40 +0200

    Initial Commit

---
+++ Boozemeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boozemeter
+//
+//  Created by Volodymyr Hryhoriev on 11/4/17.
+//  Copyright © 2017 NoblesTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
