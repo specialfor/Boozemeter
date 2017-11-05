53f2ab7960b3246028f834061bb91f377abac3c0
Volodymyr Hryhoriev <v.hryhoriev@tecsynt.com>
2017-11-05 13:30:10 +0200

    Add 'Drinks'

--- Boozemeter/Library/Models/Drink.swift
+++ Boozemeter/Library/Models/Drink.swift
@@ -14,6 +14,14 @@ struct Drink {
     
     static let drinks: [Drink] = [
         Drink(imageName: "vodka", title: "Водка", turnover: 40, size: 50),
-        Drink(imageName: "wine", title: "Вино", turnover: 12, size: 150),
-        ]
+        Drink(imageName: "beer", title: "Пиво", turnover: 5, size: 500),
+        Drink(imageName: "champage", title: "Шампанское", turnover: 11, size: 200),
+        Drink(imageName: "cognac", title: "Коньяк", turnover: 40, size: 40),
+        Drink(imageName: "gin", title: "Джин", turnover: 40, size: 40),
+        Drink(imageName: "liquer", title: "Ликёр", turnover: 40, size: 40),
+        Drink(imageName: "rum", title: "Ром", turnover: 40, size: 40),
+        Drink(imageName: "tequila", title: "Текила", turnover: 40, size: 40),
+        Drink(imageName: "vermouth", title: "Вермут", turnover: 18, size: 200),
+        Drink(imageName: "whisky", title: "Виски", turnover: 40, size: 40),
+        Drink(imageName: "wine", title: "Вино", turnover: 17, size: 200)]
 }
